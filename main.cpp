240d6285258586d4d8bc0b84aa537fd35433f8d8
Anti303 <moonjitso@gmail.com>
2025-11-06 16:02:22 -0600

    addded getComputermove function that randomizes the computer output of rock, paper, scissors.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string getComputerMove(int move){
+    if (move == 0) {
+        return "rock";
+    }
+    else if (move == 1) {
+        return "paper";
+    }
+    return "scissors";
+}
 
 int main() {
-
-    
+    srand(time(0));
+    string compMove = getComputerMove(rand() % 3);
+    string compMove2 = getComputerMove(rand() % 3);
+    cout << compMove << "\n" << compMove2;
 }
